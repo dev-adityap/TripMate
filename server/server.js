f0b99c3f0fcd080e9a9fff952f1de34ec78f0b13
dev-adityap <aditya.jobinboxhere@gmail.com>
2026-09-14 00:07:25 +0530

    Secure environment variables

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
 app.use(express.json());
 
 // Replace with your MongoDB Atlas connection string
-const MONGO_URI = "mongodb://adityapanna009_db_user:g2156ty5007361ad21A@ac-gwzppwq-shard-00-00.jspackf.mongodb.net:27017,ac-gwzppwq-shard-00-01.jspackf.mongodb.net:27017,ac-gwzppwq-shard-00-02.jspackf.mongodb.net:27017/tripmate?ssl=true&replicaSet=atlas-9dcm5d-shard-0&authSource=admin&appName=Cluster0";
+const MONGO_URI = process.env.MONGO_URI;
 mongoose.connect(MONGO_URI)
   .then(() => console.log('Connected to MongoDB Atlas successfully'))
   .catch(err => console.error('MongoDB connection error:', err));
